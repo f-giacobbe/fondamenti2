e9a95e9db6c9f73e32e233489bab26c512b40d0e
Francesco Giacobbe <francescogiacob11@live.it>
2025-07-22 13:24:34 +0200

    Quick Sort fix

--- Lezione20/QuickSort.java
+++ Lezione20/QuickSort.java
@@ -1,28 +1,27 @@
 package Lezione20;
 
+import java.util.Arrays;
+
 public class QuickSort {
     public static void quickSort(int[] a) {
         if (a == null || a.length == 0) {
             return;
         }
 
-        quickSort(a, 0, a.length-1);
-        return;
+        quickSort(a, 0, a.length - 1);
     }
 
     private static void quickSort(int[] a, int low, int high) {
+        if (low >= high)
+            return;
+
         int pivotIndex = partition(a, low, high);
         /* adesso oltre a conoscere l'indice del pivot, alla sua sinistra avremo solo elementi minori di
          * esso, mentre alla sua destra solo elementi maggiori, quindi non ci basta che ordinare le due parti
          */
 
-        if (low < pivotIndex-1) {
-            quickSort(a, low, pivotIndex-1);
-        }
-
-        if (pivotIndex < high) {
-            quickSort(a, pivotIndex, high);
-        }
+        quickSort(a, low, pivotIndex - 1);
+        quickSort(a, pivotIndex + 1, high);
     }
 
     /**
@@ -30,19 +29,28 @@ public class QuickSort {
      * uguali del valore pivot, e sulla destra gli elementi maggiori o uguali del valore pivot.
      * Utilizza come pivot il valore in posizione mediana della porzione passata come input
      * @param a L'array contenente il sottoarray da partizionare
-     * @param i L'estremo sinistro del sottoarray
-     * @param j L'estremo destro del sottoarray
+     * @param low L'estremo sinistro del sottoarray
+     * @param high L'estremo destro del sottoarray
      * @return La posizione assunta dal valore pivot al termine del partizionamento
      */
-    private static int partition(int[] a, int i, int j) {
+    private static int partition(int[] a, int low, int high) {
         int tmp;
-        int pivot = a[(i+j)/2];
+        int mid = (low + high) / 2;
+        int pivot = a[mid];
+
+        // Swap tra pivot e ultimo elemento
+        tmp = a[mid];
+        a[mid] = a[high];
+        a[high] = tmp;
+
+        int i = low;
+        int j = high - 1;
 
         while (i <= j) {
-            while (a[i] < pivot)    //trova numero maggiore del pivot
+            while (i <= j && a[i] < pivot)    //trova numero maggiore del pivot
                 i++;
 
-            while (a[j] > pivot)    //trova numero minore del pivot
+            while (i <= j && a[j] > pivot)    //trova numero minore del pivot
                 j--;
 
             if (i <= j) {
@@ -53,6 +61,10 @@ public class QuickSort {
                 j--;
             }
         }
+
+        tmp = a[i];
+        a[i] = a[high];
+        a[high] = tmp;
 
         return i;
     }
